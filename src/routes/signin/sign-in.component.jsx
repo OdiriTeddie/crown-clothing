201066809c83bc37783fa3bc65668d9ae53c2cae
Odiri Teddie <odiriteddie.adamu@gmail.com>
2023-01-11 19:06:04 -0500

    fix: fixed the sign-in button syntax error

--- src/routes/signin/sign-in.component.jsx
+++ src/routes/signin/sign-in.component.jsx
@@ -1,12 +1,15 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
+
+import { signInWithGooglePopup, signInWithGoogleRedirect, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
+
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
+
 
 const Signin = () => {
+
     const logGoogleUser =  async() => {
         const {user} = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
     }
-
-
 
     return(
         <div>
@@ -14,6 +17,7 @@ const Signin = () => {
             <button onClick= {logGoogleUser}>
                 Sign in with Google Popup
             </button>
+            <SignUpForm />
         </div>
     )
 };
